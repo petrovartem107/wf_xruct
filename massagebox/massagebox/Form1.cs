cc7903da38b83f046c0a8438303a7c5732dc40ca
LAPTOP-U10RCJLI\а715 <petrovartem353@gmail.com>
2022-05-24 19:06:19 +0300

    Sdelal_MessageBox

---
+++ massagebox/massagebox/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace massagebox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Нормали?", "Обработка", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                label1.Text = "Кросс";
+            }
+            else
+            {
+                label1.Text = "ЛОХ";
+            }
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
